5aa554a9e33454696251e139c805c7030741015d
nicolasgonzalez <ngonzalezromero@gmail.com>
2021-10-06 10:41:30 -0300

    test log

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "lambaa"
 import pingController from "./controllers/pingController"
 
+console.log("hola nico")
+
 const router = new Router().registerController(new pingController())
 
 export const handler = router.getHandler()
