0dafa2d637ec15048460de78d4a49427413b5721
franbri <79385600+franbri@users.noreply.github.com>
2021-11-10 10:31:14 -0300

    test2 (#10)
    * now get messageCount works
    
    * added DL info to /queues request
    
    * now get messageCount works
    
    * added DL info to /queues request
    
    * modularized some sqs functions
    
    * now working promises
    
    * added DL functions
    
    * messages working !?

--- src/controllers/queueController.ts
+++ src/controllers/queueController.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, SQSHandler } from "aws-lambda"
-import { Controller, GET, Use, Schedule, POST,FromPath } from "lambaa"
+import { Controller, GET, Use, Schedule, POST, FromPath } from "lambaa"
+
 
 import queue from "../subcontrollers/queue";
 
@@ -7,22 +8,25 @@ import queue from "../subcontrollers/queue";
 export default class queueController {
 
     @GET("/queues")
-    async getQueues(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>{
+    async getQueues(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
         var sqs = new queue();
         var ret = await sqs.listQueues();
         console.log(ret);
-        return {  
+        return {
+
             statusCode: 200,
             body: JSON.stringify(ret)
         }
     }
 
     @GET("/{queueid}")
-    public async getQueueInfoByID(event: APIGatewayProxyEvent, @FromPath("queueid")queueid:string): Promise<APIGatewayProxyResult> {
+    public async getQueueInfoByID(event: APIGatewayProxyEvent, @FromPath("queueid") queueid: string): Promise<APIGatewayProxyResult> {
+
         var ret = {
             statusCode: 200,
             body: "error"
         }
+
 
         var sqs = new queue();
         var name = await sqs.getQueueURL(queueid);
@@ -33,6 +37,15 @@ export default class queueController {
         return ret;
     }
 
+        var sqs = new queue();
+        var name = await sqs.getQueueURL(queueid);
+
+        if (name) {
+            ret.body = await sqs.getQueueInfoByURL(name.QueueUrl);
+        }
+        return ret;
+    }
+    /*
     @GET("/group/{GroupName}")
     public getQueueGroup(event: APIGatewayProxyEvent): APIGatewayProxyResult {
         return {
@@ -40,9 +53,11 @@ export default class queueController {
             body: "no groups defined",
         }
     }
-
+    */
     @GET("/errors")
-    public getFailedQueues(event: APIGatewayProxyEvent): APIGatewayProxyResult {
+    public getFailedQueues(
+        event: APIGatewayProxyEvent
+    ): APIGatewayProxyResult {
 
         return {
             statusCode: 200,
@@ -61,13 +76,17 @@ export default class queueController {
     }*/
 
     @POST("/purge/{queueid}")
-    public async purgeQueue(event: APIGatewayProxyEvent,@FromPath("queueid")queueid:string): Promise<APIGatewayProxyResult>  {
+    public async purgeQueue(
+        event: APIGatewayProxyEvent,
+        @FromPath("queueid") queueid: string
+    ): Promise<APIGatewayProxyResult> {
+
         var sqs = new queue();
         var ret = sqs.purgeQueue(queueid);
 
         return ret;
     }
 
-    
+
 
 }
